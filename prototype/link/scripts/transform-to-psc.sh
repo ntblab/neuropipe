484e10eaae182cf8387c4e489f5dec2c1527d63a
Alexa <atompary@gmail.com>
2012-04-12 15:16:43 -0400

    removing scaling factor variable; changing way we use temp files

--- prototype/link/scripts/transform-to-psc.sh
+++ prototype/link/scripts/transform-to-psc.sh
@@ -5,9 +5,9 @@ set -e
 
 source globals.sh
 
-if [ $# -ne 4 ]; then
+if [ $# -ne 5 ]; then
   echo "
-  usage: `basename $0` input mean_image mask_image output
+  usage: `basename $0` input mean_image mask_image output scaling_factor
 
   transforms the given image of beta values into an image of % signal change,
   given an appropriate scaling factor (which you must determine), mean signal
@@ -24,10 +24,11 @@ input_image=$1
 mean_image=$2
 mask_image=$3
 output_image=$4
+scaling_factor=$5
 
 
 fslmaths "$input_image" \
-         -mul "$SCALING_FACTOR" \
+         -mul "$scaling_factor" \
          -div "$mean_image" \
          -mul "$mask_image" \
          -mul "100.0" \
--- scripts/conjunction-analysis.sh
+++ scripts/conjunction-analysis.sh
@@ -3,51 +3,43 @@
 # Author: Alexa Tompary
 # Last edited: 3/7/11
 
-if [ $# -ne 4 ]; then
+if [ $# -lt 4 ]; then
   echo "
-usage: `basename $0` path/to/stat_vol1 path/to/stat_vol2 threshold path/to/output_vol
+usage: `basename $0` path/to/outputvol thresh path/to/stat_vol2 path/to/stat_vol1 path/to/stat_vol2 [path/to/statvol3] ...
 
-This script runs a conjunction analysis on two feat contrasts (in NIFTI format).
-Enter the zstat volumes and the output volume with the nii.gz extension, and enter 
-the feat directory where your stat volumes are located.
-Your final volume will land in the same directory.
+This script runs a conjunction analysis on feat contrasts (in NIFTI format).
+Enter the zstat volumes and the output volume with the nii.gz extension. Make
+sure to include paths to each stat volume and also the output volume.
   "
   exit
 fi
 
-source globals.sh
+thresh=$2
 
-vol1=$1
-vol2=$2
-thresh=$3
-outputvol=$4
+outputdir=`dirname $1`
 
-outputdir=`dirname $outputvol`
-
-for statvol in $vol1 $vol2; do
-	i=$(basename $statvol)
-	#get sign maps of each contrast -- if there's an easier way to do it, please let me know!
-	fslmaths $statvol -recip ${outputdir}/recip_$i
-	fslmaths ${outputdir}/recip_$i -abs ${outputdir}/recip_abs_$i
-	fslmaths ${outputdir}/recip_abs_$i -mul $statvol ${outputdir}/sign_map_$i
-	#absolute value of contrasts, to be masked later
-	fslmaths $statvol -abs ${outputdir}/abs_$i
+for ((statvol=3; statvol<=$# - 1; statvol++)); do
+	nextvol=$((statvol + 1))
+	if [ $statvol -eq 3 ]; then outputvol=${!statvol}; else outputvol=$1; fi
+	for vol in $outputvol ${!nextvol}; do
+		i=$(basename $vol)
+		fslmaths $vol -recip ${outputdir}/recip_$i
+		fslmaths ${outputdir}/recip_$i -abs ${outputdir}/recip_abs_$i 
+		fslmaths ${outputdir}/recip_abs_$i -mul $vol ${outputdir}/sign_map_$i 	#get sign maps of each map
+		fslmaths $vol -abs ${outputdir}/abs_$i 	#abs value of map, to be masked later
+	done
+	
+	# multiply sign maps together to find voxels that share sign for each contrast, zero out all others
+	fslmaths ${outputdir}/sign_map_$(basename $outputvol) -mul ${outputdir}/sign_map_$(basename ${!nextvol}) ${outputdir}/sign_map_combined
+	fslmaths ${outputdir}/sign_map_combined -thr 0 ${outputdir}/sign_map_thr
+	
+	# get the lower intensity of the two contrasts (abs valued) for each voxel, and then threshold the map at the specified level
+	fslmerge -t ${outputdir}/stats_concat ${outputdir}/abs_$(basename $outputvol) ${outputdir}/abs_$(basename ${!nextvol})
+	fslmaths ${outputdir}/stats_concat.nii.gz -Tmin -thr $thresh ${outputdir}/stats_thr
+	
+	# revert back to original signs
+	fslmaths ${outputdir}/stats_thr.nii.gz -mul ${outputdir}/sign_map_$(basename ${!nextvol}) $1
 done
 
-#multiply sign maps together to find voxels that share sign for each contrast, zero out all others
-fslmaths ${outputdir}/sign_map_$(basename $vol1) -mul ${outputdir}/sign_map_$(basename $vol2) ${outputdir}/sign_map
-fslmaths ${outputdir}/sign_map.nii.gz -thr 0 ${outputdir}/sign_map_thr
-
-#get the lower intensity of the two contrasts (abs valued) for each voxel, and then threshold the map at the requested level
-fslmerge -t ${outputdir}/stats_concat ${outputdir}/abs_$(basename $vol1) ${outputdir}/abs_$(basename $vol2)
-fslmaths ${outputdir}/stats_concat.nii.gz -Tmin ${outputdir}/stats_min
-fslmaths ${outputdir}/stats_min.nii.gz -thr $thresh ${outputdir}/stats_thr
-
-#revert back to original signs
-fslmaths ${outputdir}/stats_thr.nii.gz -mul ${outputdir}/sign_map_$(basename $vol1) $outputvol
-
 #clean up
-rm -rf ${outputdir}/abs_*
-rm -rf ${outputdir}/sign_map*
-rm -rf ${outputdir}/stats_*
-rm -rf ${outputdir}/recip_*
+rm -rf ${outputdir}/abs_* ${outputdir}/sign_map* ${outputdir}/stats_* ${outputdir}/recip_*
